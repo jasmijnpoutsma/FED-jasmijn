b7d07fab26d4148b2822bd21c91672aad85e6ac9
jasmijnpoutsma <jasmijnpoutsma@icloud.com>
2025-12-10 20:54:50 +0100

    Add files via upload

--- java/javascript.js
+++ java/javascript.js
@@ -1,11 +1,37 @@
 // carrousel knoppen 
+// const deDiamantjesKnop = document.querySelector("section:nth-of-type(3) ul:nth-of-type(1) li:nth-of-type(2) ");
+
+// deDiamantjesKnop.onclick = veranderCarrousel
+
+// function veranderCarrousel() {
+//     alert("he"); 
+// }
+
 const deDiamantjesKnop = document.querySelector("section:nth-of-type(3) ul:nth-of-type(1) li:nth-of-type(2) ");
 
 deDiamantjesKnop.onclick = veranderCarrousel
-
+ 
 function veranderCarrousel() {
-    alert("he"); 
-}
+ 
+// die leather sectie ophalen
+
+ const leatherSection = document.querySelector("section:nth-of-type(3) ul:nth-of-type(2)");
+ 
+// leader sectie verborgen maken door een class hidden toe te voegen en in css die display:none te zetten
+
+ leatherSection.classList.add('hidden')
+
+ }
+
+
+
+
+
+
+
+
+
+ 
 
 // hamburger menu
 const hamburgerMenu = document.querySelector("header nav:nth-of-type(1) ul:nth-of-type(2)");
