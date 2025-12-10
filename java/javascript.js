031593772acae426887edd05e2630892a73b1a41
jasmijnpoutsma <jasmijnpoutsma@icloud.com>
2025-12-10 22:12:38 +0100

    Add files via upload

--- java/javascript.js
+++ java/javascript.js
@@ -7,23 +7,43 @@
 //     alert("he"); 
 // }
 
-const deDiamantjesKnop = document.querySelector("section:nth-of-type(3) ul:nth-of-type(1) li:nth-of-type(2) ");
+// const deDiamantjesKnop = document.querySelector("section:nth-of-type(3) ul:nth-of-type(1) li:nth-of-type(2) ");
 
-deDiamantjesKnop.onclick = veranderCarrousel
+// deDiamantjesKnop.onclick = veranderCarrousel
  
-function veranderCarrousel() {
+// function veranderCarrousel() {
  
-// die leather sectie ophalen
+// // die leather sectie ophalen
+
+//  const leatherSection = document.querySelector("section:nth-of-type(3) ul:nth-of-type(1)");
+ 
+// // leader sectie verborgen maken door een class hidden toe te voegen en in css die display:none te zetten
+
+//  leatherSection.classList.add('hidden')
+
+//  }
+
+const deLeatherKnop = document.querySelector("section:nth-of-type(3) div button:nth-of-type(1)");
+const deDiamantjesKnop = document.querySelector("section:nth-of-type(3) div button:nth-of-type(2)");
+ 
+deLeatherKnop.onclick = veranderCarrouselLeather
+deDiamantjesKnop.onclick = veranderCarrouselDiamant
+ 
+function veranderCarrouselLeather() {
 
  const leatherSection = document.querySelector("section:nth-of-type(3) ul:nth-of-type(1)");
- 
-// leader sectie verborgen maken door een class hidden toe te voegen en in css die display:none te zetten
 
- leatherSection.classList.add('hidden')
+ leatherSection.classList.add("hidden")
 
  }
+ 
+function veranderCarrouselDiamant() {
 
+    const DiamantSection = document.querySelector("section:nth-of-type(3) ul:nth-of-type(2)");
 
+ DiamantSection.classList.add("hidden")
+
+ }
 
 
 
