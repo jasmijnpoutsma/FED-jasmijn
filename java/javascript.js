f172203f5b2a2ad126f11e156db712c017364137
jasmijnpoutsma <jasmijnpoutsma@icloud.com>
2025-12-11 11:34:11 +0100

    Add files via upload

--- java/javascript.js
+++ java/javascript.js
@@ -1,63 +1,52 @@
 // carrousel knoppen 
-// const deDiamantjesKnop = document.querySelector("section:nth-of-type(3) ul:nth-of-type(1) li:nth-of-type(2) ");
-
-// deDiamantjesKnop.onclick = veranderCarrousel
-
-// function veranderCarrousel() {
-//     alert("he"); 
-// }
-
-// const deDiamantjesKnop = document.querySelector("section:nth-of-type(3) ul:nth-of-type(1) li:nth-of-type(2) ");
-
-// deDiamantjesKnop.onclick = veranderCarrousel
- 
-// function veranderCarrousel() {
- 
-// // die leather sectie ophalen
-
-//  const leatherSection = document.querySelector("section:nth-of-type(3) ul:nth-of-type(1)");
- 
-// // leader sectie verborgen maken door een class hidden toe te voegen en in css die display:none te zetten
-
-//  leatherSection.classList.add('hidden')
-
-//  }
 
 const deLeatherKnop = document.querySelector("section:nth-of-type(3) div button:nth-of-type(1)");
-
- const deDiamantjesKnop = document.querySelector("section:nth-of-type(3) div button:nth-of-type(2)");
+const deDiamantjesKnop = document.querySelector("section:nth-of-type(3) div button:nth-of-type(2)");
  
+
 deLeatherKnop.onclick = veranderCarrouselLeather;
-
- deDiamantjesKnop.onclick = veranderCarrouselDiamant;
+deDiamantjesKnop.onclick = veranderCarrouselDiamant;
  
  
-  const DiamantSection = document.querySelector("section:nth-of-type(3) ul:nth-of-type(1)");
-
-   const leatherSection = document.querySelector("section:nth-of-type(3) ul:nth-of-type(2)");
+const DiamantSection = document.querySelector("section:nth-of-type(3) ul:nth-of-type(1)");
+const leatherSection = document.querySelector("section:nth-of-type(3) ul:nth-of-type(2)");
  
 function veranderCarrouselLeather() {
 
-   leatherSection.classList.add("hidden");
+    leatherSection.classList.add("hidden");
+    DiamantSection.classList.remove("hidden");
 
-   DiamantSection.classList.remove("hidden")
+    deDiamantjesKnop.classList.add("not-active");
+    deLeatherKnop.classList.remove("not-active")
 
  }
  
 function veranderCarrouselDiamant() {
  
-  DiamantSection.classList.add("hidden");
+    DiamantSection.classList.add("hidden");
+    leatherSection.classList.remove("hidden");
 
-   leatherSection.classList.remove("hidden")
+    deLeatherKnop.classList.add("not-active")
+    deDiamantjesKnop.classList.remove("not-active");
+
+ }
  
-}
-
-
-
-
-
 
 
 
 // hamburger menu
-const hamburgerMenu = document.querySelector("header nav:nth-of-type(1) ul:nth-of-type(2)");
+const openHamburgerMenu = document.querySelector("header nav:nth-of-type(1) li:nth-of-type(1) button");
+const sluitHamburgerMenu = document.querySelector("header nav:nth-of-type(2) ul button");
+const deNav = document.querySelector("header nav:nth-of-type(2)");
+
+openHamburgerMenu.onclick = openMenu;
+sluitHamburgerMenu.onclick = sluitMenu;
+
+function openMenu() {
+    // alert("he");
+   deNav.classList.add("is-open");
+}
+
+function sluitMenu() {
+   deNav.classList.remove("is-open");
+}
