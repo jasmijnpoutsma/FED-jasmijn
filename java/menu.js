6ea43b7d59e0129da239f682cc1af585755eda8e
jasmijnpoutsma <jasmijnpoutsma@icloud.com>
2025-12-11 14:44:05 +0100

    Add files via upload

---
+++ java/menu.js
@@ -0,0 +1,18 @@
+
+
+// hamburger menu
+const openHamburgerMenu = document.querySelector("header nav:nth-of-type(1) li:nth-of-type(1) button");
+const sluitHamburgerMenu = document.querySelector("header nav:nth-of-type(2) ul button");
+const deNav = document.querySelector("header nav:nth-of-type(2)");
+
+openHamburgerMenu.onclick = openMenu;
+sluitHamburgerMenu.onclick = sluitMenu;
+
+function openMenu() {
+    // alert("he");
+   deNav.classList.add("is-open");
+}
+
+function sluitMenu() {
+   deNav.classList.remove("is-open");
+}
---
+++ java/script.js
@@ -0,0 +1,51 @@
+// carrousel knoppen 
+// snap ik nieet helemaal, wel deels
+const deLeatherKnop = document.querySelector("section:nth-of-type(3) div button:nth-of-type(1)");
+const deDiamantjesKnop = document.querySelector("section:nth-of-type(3) div button:nth-of-type(2)");
+ 
+
+deLeatherKnop.onclick = veranderCarrouselLeather;
+deDiamantjesKnop.onclick = veranderCarrouselDiamant;
+ 
+ 
+const DiamantSection = document.querySelector("section:nth-of-type(3) ul:nth-of-type(1)");
+const leatherSection = document.querySelector("section:nth-of-type(3) ul:nth-of-type(2)");
+ 
+function veranderCarrouselLeather() {
+
+    leatherSection.classList.add("hidden");
+    DiamantSection.classList.remove("hidden");
+
+    deDiamantjesKnop.classList.add("not-active");
+    deLeatherKnop.classList.remove("not-active")
+
+ }
+ 
+function veranderCarrouselDiamant() {
+ 
+    DiamantSection.classList.add("hidden");
+    leatherSection.classList.remove("hidden");
+
+    deLeatherKnop.classList.add("not-active")
+    deDiamantjesKnop.classList.remove("not-active");
+
+ }
+ 
+
+
+
+// hamburger menu
+const openHamburgerMenu = document.querySelector("header nav:nth-of-type(1) li:nth-of-type(1) button");
+const sluitHamburgerMenu = document.querySelector("header nav:nth-of-type(2) ul button");
+const deNav = document.querySelector("header nav:nth-of-type(2)");
+
+openHamburgerMenu.onclick = openMenu;
+sluitHamburgerMenu.onclick = sluitMenu;
+
+function openMenu() {
+   deNav.classList.add("is-open");
+}
+
+function sluitMenu() {
+   deNav.classList.remove("is-open");
+}
